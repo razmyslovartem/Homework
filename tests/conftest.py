84b719b4b79622650eac5c7dbf3fcc7f1a146407
Artem Razmyslov <89083284426@mail.ru>
2026-01-25 14:08:19 +0300

    add pytest-cov

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import fixture
 
 
@@ -5,3 +6,15 @@ from pytest import fixture
 def numbers():
     return [1, 2, 3, 4]
 
+
+@pytest.mark.parametrize('word, correct', [('шалаш', True),
+                                           ('привет', False),
+                                           ('казак', True),
+                                           ('ура', False)])
+def test_is_palindrom(word, correct):
+    assert is_palindrom(word) == correct
+
+
+def test_error_is_palindrom():
+    with pytest.raises(TypeError):
+        is_palindrom(1)
