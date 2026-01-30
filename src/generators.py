740a87cad096076dbc576c613c2bdd3520e69666
razmyslovartem <razmyslovartem@icloud.com>
2026-01-30 12:13:38 +0300

    update test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,129 +1,59 @@
 """
-Модуль generators.py содержит
+В модуле generators.py
 генераторы для обработки данных транзакций.
 """
 
 from typing import Iterator
 
 
-def filter_by_currency(full_transactions: list, filter_name: str) -> Iterator:
+def filter_by_currency(data_array_transactions: list[dict], filter_currency: str) -> Iterator:
     """
-    Принимает на вход список словарей, представляющих транзакции.
-    Возвращать итератор, который поочередно выдает транзакции, где валюта
-    операции соответствует заданной.
+    Принимает массив данных в виде списка транзакций и возвращает итератор ленивых
+    запросов, который поочередно выдает отфильтрованные по виду валюты транзакции.
     """
-    for report in full_transactions:
-        try:
-            currency_name = report["operationAmount"]["currency"]["name"]
-            if currency_name == filter_name:
-                yield report
-        except (KeyError, TypeError):
+    for transaction_report in data_array_transactions:
+
+        currency_transaction = transaction_report["operationAmount"]["currency"]["name"]
+
+        if currency_transaction == filter_currency:
+            yield transaction_report
+
+        else:
             continue
 
 
-def transaction_descriptions(full_transactions: list) -> Iterator:
+def transaction_descriptions(data_array_transactions: list[dict]) -> Iterator:
     """
-    Принимает список словарей с транзакциями и возвращает описание
-    каждой операции по очереди.
+    Принимает массив данных в виде списка транзакций и возвращает описание
+    каждой операции по очереди через генератор ленивых запросов.
     """
-    for report in full_transactions:
-        descript_transaction = report["description"]
-        try:
-            yield descript_transaction
-        except (KeyError, TypeError):
-            continue
+    for transaction_report in data_array_transactions:
+        yield transaction_report["description"]
 
 
-def number_generator(start: int = 1) -> Iterator:
-    """Генератор 1-16 значных чисел."""
-    limit_level = 9999999999999999  # Лимит 16-ти значного числа.
-    while start <= limit_level:
-        yield start
-        start += 1
+def numbers_generator(generated_value: int = 1) -> Iterator:
+    """Генератор чисел, используется в def card_number_generator."""
+
+    maximum_value = 9999999999999999
+
+    while generated_value <= maximum_value:
+        yield generated_value
+        generated_value += 1
 
 
-def card_number_generator(start: int, end: int, separator: str = " ") -> Iterator:
+def card_number_generator(start_card_number: int, finish_card_number: int, separator: str = " ") -> Iterator:
     """
-    Генерирует числа в формате XXXX XXXX XXXX XXXX, где X — цифры номера карты,
-    диапазоне генерации 0000 0000 0000 0001 до 9999 9999 9999 9999.
-    Функция принимает начальное и конечное значения для генерации всего диапазона номеров.
+    Генерирует номера карт в диапазоне 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    В формате 4 группы по 4 цифры, разделитель по умолчанию пробел.
+    Функция принимает начальное и конечное значение генераций, 3й показатель(разделитель) необязателен.
     """
-    for number in number_generator(start):
-        if number > end:
+    for generated_card_number in numbers_generator(start_card_number):
+
+        if generated_card_number > finish_card_number:
             break
 
-        format_number = f"{number:016d}"  # Форматирование через f-строку.
-        num_mask = separator.join([format_number[num : num + 4] for num in range(0, len(format_number), 4)])
-        yield num_mask
+        account_length = f"{generated_card_number:016d}"  # Добавление нулей форматированием до 16 разрядов.
 
+        format_card_number = separator.join([account_length[num : num + 4] for num in range(0, len(account_length), 4)])
 
-# # Примеры использования использования функций из модуля generators.py.
-# transactions = [
-#     {
-#         "id": 939719570,
-#         "state": "EXECUTED",
-#         "date": "2018-06-30T02:08:58.425572",
-#         "operationAmount": {
-#             "amount": "9824.07",
-#             "currency": {
-#                 "name": "USD",
-#                 "code": "USD"
-#                 }
-#             },
-#         "description": "Перевод организации",
-#         "from": "Счет 75106830613657916952",
-#         "to": "Счет 11776614605963066702",
-#     },
-#     {
-#         "id": 142264268,
-#         "state": "EXECUTED",
-#         "date": "2019-04-04T23:20:05.206878",
-#         "operationAmount": {
-#             "amount": "79114.93",
-#             "currency": {
-#                 "name": "USD",
-#                 "code": "USD"
-#                   }
-#               },
-#         "description": "Перевод со счета на счет",
-#         "from": "Счет 19708645243227258542",
-#         "to": "Счет 75651667383060284188",
-#     },
-#     {
-#         "id": 158764563,
-#         "state": "EXECUTED",
-#         "date": "2019-04-04T23:20:05.206878",
-#         "operationAmount": {
-#             "amount": "1000.00",
-#             "currency": {
-#                 "name": "RUB",
-#                 "code": "RUB"
-#                   }
-#               },
-#         "description": "Перевод с карты на карту",
-#         "from": "Счет 16808858243227258287",
-#         "to": "Счет 76951664978790527195",
-#     },
-# ]
-#
-# if __name__ == "__main__":
-#     # Получаем генератор транзакций.
-#     result_filter = filter_by_currency(transactions, 'USD')
-#
-#     # Выводим все подходящие транзакции.
-#     for currency_name in result_filter:
-#         print(
-#             f"ID: {currency_name['id']},"
-#             f"Сумма: {currency_name['operationAmount']['amount']} {currency_name['operationAmount']['currency']['name']}"
-#         )
-#
-# descriptions = transaction_descriptions(transactions)
-# for _ in range(3):
-#     print(next(descriptions))
-#
-# num = number_generator(9999999999999999)
-# print(next(num))
-# # print(next(num)) # StopIteration
-#
-# for card_number in card_number_generator(7079234523434560, 7079234523434567):
-#     print(card_number)
+        yield format_card_number
