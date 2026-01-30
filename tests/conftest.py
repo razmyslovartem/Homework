b028931547b30f785891327ff9836f50a11df41b
razmyslovartem <razmyslovartem@icloud.com>
2026-01-30 10:03:20 +0300

    update confest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -145,7 +145,7 @@ def operations_with_missing_dates(test_operations_data: List[Dict[str, Any]]) ->
 
 @pytest.fixture
 def operations_with_invalid_dates(
-    test_operations_data: List[Dict[str, Any]], invalid_get_date_cases: List[Dict[str, Any]]
+        test_operations_data: List[Dict[str, Any]], invalid_get_date_cases: List[Dict[str, Any]]
 ) -> List[Dict[str, Any]]:
     """Фикстура предоставляет операции с некорректными датами на основе test_operations_data"""
     modified_data = test_operations_data.copy()
@@ -163,3 +163,86 @@ def operations_with_invalid_dates(
 def single_operation(test_operations_data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     """Фикстура предоставляет список с одной операцией на основе test_operations_data"""
     return [test_operations_data[0].copy()]
+
+
+@pytest.fixture
+def fixture_full_transactions() -> list[dict]:
+    """Фикстура примера входных данных"""
+    transactions = [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "",
+                    "code": ""
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+    return transactions
