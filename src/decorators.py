10d0a270ec85b29cdce1c16366736dcf422d0c93
Artem Razmyslov <89083284426@mail.ru>
2026-02-10 21:01:34 +0300

    update decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -11,32 +11,25 @@ log_file = "mylog.txt"
 
 
 def log(filename: str | None = None) -> Callable:
-
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-
             timer_start = datetime.now()
             func_name: str = str(func.__name__)
             message = f"{func_name} ok"
-
             try:
                 result = func(*args, **kwargs)
                 return result
-
             except ValueError as error_1:
                 message = f"{func_name} error: {error_1}. Inputs: {args}, {kwargs}"
                 raise
-
             except Exception as error_2:
                 message = f"{func_name} unexpected error: {error_2}. Inputs: {args}, {kwargs}"
                 raise
-
             finally:
                 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 timer_delta = datetime.now() - timer_start
                 data_log = f"{timestamp} {message} {timer_delta.microseconds} мксек\n"
-
                 if filename:
                     with open(filename, "a", encoding="utf-8") as file:
                         file.write(data_log)
